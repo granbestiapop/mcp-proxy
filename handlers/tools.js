622a113f15b4fd169ca9ab3ee91742d5ea443be7
Pablo Codeiro <142818723+PabloCodeiroJusto@users.noreply.github.com>
2025-03-14 16:56:44 -0300

    feat: tool to prompt wrapper (#1)

---
+++ handlers/tools.js
@@ -0,0 +1,43 @@
+const HOST_URL = process.env.HOST_URL;
+
+export async function handleTools (request) {
+    process.stderr.write("Handling tool/get request\n" + typeof request);
+
+    const req = await fetch(`${HOST_URL}/prompts/execute`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            "method": "prompts/get",
+            "params": {
+                "name": request.params.name,
+                "arguments": {
+                    "appName": request.params.arguments.appName
+                }
+            }
+        }),
+    });
+
+    if (req.status === 500) {
+        return {
+            content: [{ type: "text", text: "Error executing prompt (that service exists?)" }],
+            isError: true,
+        };
+    }
+
+    if (!req.ok) {
+        const body = await req.json();
+        throw new Error(
+            `HTTP error! status: ${req.status}, message: ${body.message}`,
+        );
+    }
+    const response = await req.json();
+
+    process.stderr.write("RESPOSE FROM PROMPT:\n" + JSON.stringify(response.messages[0], null, 2));
+
+    return {
+        content: [{ type: "text", text: response.messages[0].content.text }],
+        isError: false,
+    };
+}
